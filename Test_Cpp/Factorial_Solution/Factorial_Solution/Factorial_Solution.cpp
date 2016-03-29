cf70bd7820a7476047dd914699521467d131d123
Ryan Xue <auspbro@gmail.com>
2016-03-29 10:57:24 +0800

    创建静态局部变量求阶乘â

---
+++ Test_Cpp/Factorial_Solution/Factorial_Solution/Factorial_Solution.cpp
@@ -0,0 +1,24 @@
+// Factorial_Solution.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int fac(int);
+
+int main()
+{
+	for (int i = 1; i <= 5; i++)
+	{
+		cout << i << "!=" << fac(i) << endl;
+	}
+    return 0;
+}
+
+int fac(int n)
+{
+	static int f = 1;
+	f = f * n;
+	return f;
+}
